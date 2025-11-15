31b8cbd379d212894e4429c0c70295cee3df36c4
killursxlf <excgerty@gmail.com>
2025-11-16 00:28:04 +0100

    Complete lab #5

--- config/logging.php
+++ config/logging.php
@@ -127,6 +127,12 @@ return [
             'path' => storage_path('logs/laravel.log'),
         ],
 
+        'scheduler' => [
+            'driver' => 'single',
+            'path'   => storage_path('logs/scheduler.log'),
+            'level'  => 'info',
+        ],
+
     ],
 
 ];
